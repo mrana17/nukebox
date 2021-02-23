079b53ab3e9d4117c408730a0848c9f81873cb19
mrana17 <rm_a@msn.com>
2021-02-23 17:45:37 +0100

    Created Favorite Button

--- components/AudioPlayer.tsx
+++ components/AudioPlayer.tsx
@@ -1,3 +1,5 @@
+import { useRouter } from "next/Router";
+import { useEffect, useState } from "react";
 import styles from "../styles/AudioPlayer.module.css";
 
 type Props = {
@@ -5,8 +7,37 @@ type Props = {
 };
 
 export default function AudioPlayer({ audio }: Props) {
+  const router = useRouter();
+  const { id } = router.query;
+  const [favorite, setFavorite] = useState(null);
+
+  useEffect(() => {
+    if (typeof id !== "string" || favorite === null) {
+      return;
+    }
+    if (favorite) {
+      localStorage.setItem("favoriteSong", id);
+    }
+    if (!favorite) {
+      localStorage.removeItem("favoriteSong");
+    }
+  }, [favorite]);
+
+  useEffect(() => {
+    if (typeof id !== "string") {
+      return;
+    }
+    setFavorite(id === localStorage.getItem("favoriteSong"));
+  }, [id]);
+
   return (
     <figure className={styles.audioFigure}>
+      <button
+        className={styles.favoriteButton}
+        onClick={() => setFavorite(!favorite)}
+      >
+        {favorite ? "❤️" : "🖤"}
+      </button>
       <figcaption className={styles.playerText}> Listen to Music</figcaption>
       <audio controls src={audio} className={styles.audioPlayer}>
         Your browser does not support<code>Audio</code> element
