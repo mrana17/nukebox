3e50fd370a0b08547e25fdb8c1eda379c08edcc1
mrana17 <rm_a@msn.com>
2021-02-24 15:30:35 +0100

    added own react player

--- components/AudioPlayer.tsx
+++ components/AudioPlayer.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from "next/Router";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import useLocalStorage from "../hooks/useLocalsStorage";
 import styles from "../styles/AudioPlayer.module.css";
 
@@ -15,6 +15,24 @@ export default function AudioPlayer({ audio }: Props) {
     []
   );
   const favorite = favoriteSongs.includes(id);
+
+  const audioRef = useRef(new Audio(audio));
+  const [isPlaying, setIsPlaying] = useState(false);
+  const audioElement = audioRef.current;
+  const intervalRef = useRef<NodeJS.Timeout>();
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    if (isPlaying) {
+      audioElement.play();
+      intervalRef.current = setInterval(() => {
+        setProgress(audioElement.currentTime);
+      }, 2000);
+    } else {
+      clearInterval(intervalRef.current);
+      audioElement.pause();
+    }
+  }, [isPlaying]);
 
   useEffect(() => {
     if (typeof id !== "string") {
@@ -38,6 +56,16 @@ export default function AudioPlayer({ audio }: Props) {
       <button className={styles.favoriteButton} onClick={handleFavoriteClick}>
         {favorite ? "❤️" : "🖤"}
       </button>
+      <button className={styles.btn} onClick={() => setIsPlaying(!isPlaying)}>
+        <img src={isPlaying ? "/pause.svg" : "/play.svg"} />
+      </button>
+      <input
+        className={styles.duration}
+        type="range"
+        min="0"
+        max={audioElement.duration}
+        value={progress}
+      />
       <figcaption className={styles.playerText}> Listen to Music</figcaption>
       <audio controls src={audio} className={styles.audioPlayer}>
         Your browser does not support<code>Audio</code> element
